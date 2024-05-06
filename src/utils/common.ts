081c923f934f313044e6f6bfd2271c0c142a6d44
TianCheng Tang <tiancheng.ext@1m.app>
2024-05-06 11:39:32 +0800

    chore: update vite env name

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,9 +1,9 @@
 import { SnackbarOrigin } from "notistack";
 
-export const PROJECT_NAME: string =
-  import.meta.env.VITE_PUBLIC_PROJECT_NAME ?? "katana_web";
+export const COOKIE_PREFIX_NAME: string =
+  import.meta.env.VITE_PUBLIC_COOKIE_PREFIX ?? "katana_web";
 
-export const PROJECT_TOKEN_NAME = `${PROJECT_NAME}_auth_token`;
+export const PROJECT_TOKEN_NAME = `${COOKIE_PREFIX_NAME}_auth_token`;
 
 export const BASE_SERVER_URL = import.meta.env.VITE_PUBLIC_SERVER_URL;
 
@@ -24,3 +24,5 @@ export const CARD_MEDIA_HEIGHT = 232;
 export const DEFAULT_SEARCH_PRODUCT_COUNT = 150;
 
 export const DEFAULT_MENU_ITEM_ID = import.meta.env.VITE_PUBLIC_MENU_ITEM_ID;
+
+export const PRODUCT_NAME = import.meta.env.VITE_PUBLIC_PRODUCT_NAME;
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,38 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { crx, ManifestV3Export } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), crx({ manifest: manifest as ManifestV3Export })],
-  resolve: {
-    alias: {
-      "@katana-common": resolve(__dirname, "modules/katana-common"),
-      "@components": resolve(__dirname, "src/components"),
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        popup: resolve(__dirname, "popup/index.html"),
+// @ts-expect-error -- to do
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    plugins: [
+      react(),
+      crx({
+        manifest: {
+          ...manifest,
+          name: (env.VITE_PUBLIC_PROJECT_NAME || "Pear Tool") as string,
+        } as ManifestV3Export,
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@katana-common": resolve(__dirname, "modules/katana-common"),
+        "@components": resolve(__dirname, "src/components"),
+        "@utils": resolve(__dirname, "src/utils"),
       },
     },
-  },
-});
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"),
+          popup: resolve(__dirname, "popup/index.html"),
+        },
+      },
+    },
+  });
+};
