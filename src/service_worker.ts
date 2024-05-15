1b667a8dc48d4b4239854c2eb2442008c7a27671
TianCheng Tang <tiancheng.ext@1m.app>
2024-05-15 18:02:29 +0800

    chore: async select store id

--- src/service_worker.ts
+++ src/service_worker.ts
@@ -180,6 +180,9 @@ chrome.windows.onRemoved.addListener(async () => {
 
 chrome.storage.onChanged.addListener((changes, namespace) => {
   for (const [key, { oldValue, newValue }] of Object.entries(changes)) {
+    clearStoreMenus(dataProxy.stores).then(() => {
+      createStoreMenu(dataProxy.stores, "select_stores").then(() => {});
+    });
     console.log(
       `Storage key "${key}" in namespace "${namespace}" changed.`,
       `Old value was "${oldValue}", new value is "${newValue}".`,
